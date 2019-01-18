8c2d3fe831d2a9b80498a18cea6dad9b00d5f298
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-19 03:31:18 +0800

    add function to delete tweet

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import twitter
 from dotenv import load_dotenv
 from tweet_generator import generate_tweet
 
+def delete_tweet(api, screen_name):
+    statuses = api.GetUserTimeline(screen_name=screen_name)
+    for s in statuses:
+        print(s.text, s.created_at)
+        api.DestroyStatus(s.id)
+
 if __name__ == "__main__":
     dotenv_path = os.path.dirname(__file__) + '.env'
     load_dotenv(dotenv_path)
@@ -15,6 +21,8 @@ if __name__ == "__main__":
         access_token_secret=os.environ.get('ACCESS_SECRET')
     )
 
-    text = generate_tweet()
-    status = api.PostUpdate(text)
-    print(status)
+    num = 1
+    for _ in range(num):
+        text = generate_tweet()
+        status = api.PostUpdate(text)
+        print(status)
