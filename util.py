3cfadadbbf98e5318be6bd4b5722e8b9abc3020d
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-20 15:41:28 +0800

    Consider '名詞-非自立-副詞可能' part as target noun

--- util.py
+++ util.py
@@ -5,7 +5,13 @@ def create_tagger():
     return tagger
 
 def is_noun(part):
-    return '名詞-一般' in part or '名詞-固有' in part or '名詞-数' in part or '名詞-サ変接続' in part
+    # '名詞-サ変接続' は 「翻訳する」の「翻訳」など
+    # '名詞-非自立-副詞可能'は「人の上」のように「の」あとの「上」など
+    return '名詞-一般' in part \
+        or '名詞-固有' in part \
+        or '名詞-数' in part \
+        or '名詞-サ変接続' in part \
+        or '名詞-非自立-副詞可能' in part
 
 def has_part_length(word_detail):
     return len(word_detail) >= 4
