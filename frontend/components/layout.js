db0089df321af9cc915b5832b443ea99536aae8e
Kaname87 <kaname.nakanishi@bibo.com.ph>
2020-08-22 15:21:55 +0800

    Use dynamic image

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -8,12 +8,17 @@ import styles from "./layout.module.scss";
 
 const siteTitle = (text) => {
   if (text.length > 0) {
-    return `${text} - ${SITE_NAME}`
+    return `${text} - ${SITE_NAME}`;
   }
-  return SITE_NAME
-}
+  return SITE_NAME;
+};
 
-export default function Layout({ children, pageTitle='', randomIdList=[] }) {
+export default function Layout({
+  children,
+  pageTitle = "",
+  randomIdList,
+  ogpQueryString,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -21,28 +26,42 @@ export default function Layout({ children, pageTitle='', randomIdList=[] }) {
           name="description"
           content="文豪の作品の名文を元に自動生成されたテキスト集です。文章中に使用された名詞に類似する名詞をランダムに置き換えることで文章を生成しています。"
         />
-        <meta name="og:image" content="https://esebunko.vercel.app/images/ogp.png" />
+        {ogpQueryString ? (
+          <meta
+            name="og:image"
+            content={`https://esebunko.vercel.app/api/ogp?${ogpQueryString}`}
+          />
+        ) : (
+          <meta name="og:image" content="https://esebunko.vercel.app/api/ogp" />
+        )}
         <meta name="og:title" content={siteTitle(pageTitle)} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="keywords" content="文豪,名言,エセ,似非,Word2Vec" />
 
         <title>{siteTitle(pageTitle)}</title>
         <link rel="icon" href="/images/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
 
         {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-132694979-1"></script>
         <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-132694979-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag() {
                   dataLayer.push(arguments);
                 }
                 gtag("js", new Date());
-                gtag("config", "UA-132694979-1");`
-            }}
-          />
+                gtag("config", "UA-132694979-1");`,
+          }}
+        />
       </Head>
       <Header />
       <main className={styles.main}>{children}</main>
--- frontend/pages/fake_quotes/[id].js
+++ frontend/pages/fake_quotes/[id].js
@@ -1,58 +1,73 @@
-import Layout from '../../components/layout';
-import FakeQuoteCard from '../../components/fakeQuoteCard';
+import Layout from "../../components/layout";
+import FakeQuoteCard from "../../components/fakeQuoteCard";
 import TwitterIcon from "../../components/twitterIcon";
 import {
   getFakeQuote,
   getFakeQuoteIdList,
   getRandomFakeQuoteIdList,
-} from '../../lib/api'
-import { getIdPaths } from '../../lib/util'
+} from "../../lib/api";
+import { getIdPaths } from "../../lib/util";
 
-export default function FakeQuote({
-  data: {
-    fakeQuoteData,
-    randomIdList,
-  }
-}) {
+export default function FakeQuote({ data: { fakeQuoteData, randomIdList } }) {
   const {
+    author,
+    book,
+    quote,
     fakeAuthor,
     fakeBook,
     fakeQuote,
   } = fakeQuoteData;
+
+  const ogpOptions = {
+    author: author.name,
+    book: book.title,
+    quote: quote.text,
+    fakeAuthor:fakeAuthor.name,
+    fakeBook: fakeBook.title,
+    fakeQuote: fakeQuote.text,
+  };
+
+  var ogpQueryString = Object.keys(ogpOptions)
+    .map((key) => {
+      return encodeURIComponent(key) + "=" + encodeURIComponent(ogpOptions[key]);
+    })
+    .join("&");
   return (
     // Check if this affect to pageload
-    <Layout pageTitle={fakeQuoteData.fakeQuote.text} randomIdList={randomIdList}>
-      <FakeQuoteCard
-        fakeQuoteData={fakeQuoteData}
-      />
+    <Layout
+      pageTitle={fakeQuoteData.fakeQuote.text}
+      randomIdList={randomIdList}
+      ogpQueryString={ogpQueryString}
+    >
+      <FakeQuoteCard fakeQuoteData={fakeQuoteData} />
       <TwitterIcon
-          fakeAuthor={fakeAuthor}
-          fakeBook={fakeBook}
-          fakeQuote={fakeQuote}
-        />
+        fakeAuthor={fakeAuthor}
+        fakeBook={fakeBook}
+        fakeQuote={fakeQuote}
+      />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  return await getIdPaths(getFakeQuoteIdList)
+  return await getIdPaths(getFakeQuoteIdList);
 }
 
 export async function getStaticProps({ params }) {
-  const fkQuoteData = await getFakeQuote(params.id)
-  const randomData = await getRandomFakeQuoteIdList(params.id)
-  const randomIdList = randomData.id_list
+  const fkQuoteData = await getFakeQuote(params.id);
+  const randomData = await getRandomFakeQuoteIdList(params.id);
+  const randomIdList = randomData.id_list;
   const fakeQuoteData = {
     ...fkQuoteData,
     randomIdList,
-  }
+  };
 
   return {
     props: {
-      data:{
+      data: {
         fakeQuoteData,
         randomIdList,
-      }
-    }
-  }
+      },
+    },
+  };
 }
